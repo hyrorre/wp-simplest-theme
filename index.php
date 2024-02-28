c2fe1553c1a9ac5962811be486120f3e0f44bf77
hyrorre <hiromu.yamashita@hotmail.com>
2024-02-28 23:27:45 +0900

    Update: layout

--- index.php
+++ index.php
@@ -10,23 +10,32 @@
 
 <body <?php body_class(); ?>>
   <?php get_header(); ?>
-  <main>
-    <?php while (have_posts()) : ?>
+  <main class="container">
+    <?php while (have_posts()): ?>
       <article class="py-4 border-bottom">
         <?php the_post(); ?>
         <a href="<?php the_permalink(); ?>" class="link-dark text-decoration-none">
-          <h2>
-            <?php the_title(); ?>
-          </h2>
+          <h2><?php the_title(); ?></h2>
         </a>
-        <p>
-          <?php the_time('Y/m/d'); ?>
-        </p>
-        <p>
-          <?php the_category(); ?>
-          <?php the_tags('', ' '); ?>
-        </p>
+        <div class="px-1 text-secondary">
+          <div class="me-2">
+            <i class="bi bi-calendar me-2"></i>
+            <?php the_time('Y-m-d'); ?>
+          </div>
+          <div class="d-flex flex-wrap">
+            <div class="d-flex me-2">
+              <i class="bi bi-folder me-2"></i>
+              <?php the_category(); ?>
+            </div>
+            <div class="post-tags me-2">
+              <i class="bi bi-tags me-2"></i>
+              <?php the_tags('', ''); ?>
+            </div>
+          </div>
+        </div>
+        <div class="content">
         <?php the_content(); ?>
+        </div>
       </article>
     <?php endwhile; ?>
   </main>
