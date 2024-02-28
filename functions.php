9f6f9f8827c23988f6eff26d59c0c3300c78dc7d
hyrorre <hiromu.yamashita@hotmail.com>
2024-02-28 23:23:08 +0900

    Add: disable auto resize

--- functions.php
+++ functions.php
@@ -15,3 +15,9 @@ add_action('wp_enqueue_scripts', function () {
     );
     wp_enqueue_script('wp-simplest-index-js', get_theme_file_uri('/index.js'), [], false, true);
 });
+
+add_filter('intermediate_image_sizes_advanced', function ($sizes) {
+    return [];
+});
+add_filter('big_image_size_threshold', '__return_false');
+update_option('medium_large_size_w', 0);
