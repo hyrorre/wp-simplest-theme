43eba975a460561b4c39d715226b0c6a77b71851
hyrorre <hiromu.yamashita@hotmail.com>
2024-02-28 23:22:16 +0900

    Fix: functions.php

--- functions.php
+++ functions.php
@@ -3,6 +3,15 @@
 remove_action('wp_head', 'wp_generator');
 
 add_action('wp_enqueue_scripts', function () {
-  wp_enqueue_style('wp-simplest-style-css', get_theme_file_uri('/style.css'));
-  wp_enqueue_script('wp-simplest-index-js', get_theme_file_uri('/index.js'), array(), false, true);
+    wp_enqueue_style(
+        'bootstrap-icons',
+        get_theme_file_uri('/node_modules/bootstrap-icons/font/bootstrap-icons.min.css')
+    );
+    wp_enqueue_style(
+        'wp-simplest-style-css',
+        get_theme_file_uri('/style.css'),
+        [],
+        date('ymdHis', filemtime(get_theme_file_path('/style.css')))
+    );
+    wp_enqueue_script('wp-simplest-index-js', get_theme_file_uri('/index.js'), [], false, true);
 });
