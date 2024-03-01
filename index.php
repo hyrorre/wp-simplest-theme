f30b9009dc101c64c36a5a2a5fbec1d37a711575
hyrorre <hiromu.yamashita@hotmail.com>
2024-03-02 05:45:28 +0900

    Update: sidebar layout (WIP)

--- index.php
+++ index.php
@@ -10,36 +10,38 @@
 
 <body <?php body_class(); ?>>
   <?php get_header(); ?>
-  <main class="container">
-    <?php while (have_posts()): ?>
-      <article class="py-4 border-bottom">
-        <?php the_post(); ?>
-        <a href="<?php the_permalink(); ?>" class="link-dark text-decoration-none">
-          <h2><?php the_title(); ?></h2>
-        </a>
-        <div class="px-1 text-secondary">
-          <div class="me-2">
-            <i class="bi bi-calendar me-2"></i>
-            <?php the_time('Y-m-d'); ?>
-          </div>
-          <div class="d-flex flex-wrap">
-            <div class="d-flex me-2">
-              <i class="bi bi-folder me-2"></i>
-              <?php the_category(); ?>
+  <div class="container-xl flex-grow-1 d-flex flex-column flex-xl-row align-items-start">
+    <main class="container flex-grow-1">
+      <?php while (have_posts()) : ?>
+        <article class="py-4 border-bottom">
+          <?php the_post(); ?>
+          <a href="<?php the_permalink(); ?>" class="link-dark text-decoration-none">
+            <h2><?php the_title(); ?></h2>
+          </a>
+          <div class="px-1 text-secondary">
+            <div class="me-2">
+              <i class="bi bi-calendar me-2"></i>
+              <?php the_time('Y-m-d'); ?>
             </div>
-            <div class="post-tags me-2">
-              <i class="bi bi-tags me-2"></i>
-              <?php the_tags('', ''); ?>
+            <div class="d-flex flex-wrap">
+              <div class="d-flex me-2">
+                <i class="bi bi-folder me-2"></i>
+                <?php the_category(); ?>
+              </div>
+              <div class="post-tags me-2">
+                <i class="bi bi-tags me-2"></i>
+                <?php the_tags('', ''); ?>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="content">
-        <?php the_content(); ?>
-        </div>
-      </article>
-    <?php endwhile; ?>
-  </main>
-  <?php get_sidebar(); ?>
+          <div class="content">
+            <?php the_content(); ?>
+          </div>
+        </article>
+      <?php endwhile; ?>
+    </main>
+    <?php get_sidebar(); ?>
+  </div>
   <?php get_footer(); ?>
   <?php wp_footer(); ?>
 </body>
--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,5 @@
 <aside class="sidebar text-secondary border-start flex-glow-0 flex-shrink-0 my-4 mx-2 ms-lg-3 p-3">
-  <ContentDoc v-if="route.name === 'post-id'" v-slot="{ doc: page }">
+  <div class="d-none">
     <h4><i class="bi bi-list-ul me-2"></i>目次</h4>
     <ul>
       <li v-for="link of page.body.toc.links">
@@ -11,21 +11,22 @@
         </ul>
       </li>
     </ul>
-  </ContentDoc>
+  </div>
   <h4><i class="bi bi-folder me-2"></i>カテゴリ</h4>
-  <ul class="list-unstyled">
-    <li v-for="category of categories">
-      <nuxt-link :to="`/category/${category}`">{{ category }}</nuxt-link>
-    </li>
+  <ul class="ps-0">
+    <?php foreach (get_categories('get=all') as $category) : ?>
+      <li class="list-unstyled">
+        <a href="<?php echo get_category_link($category->term_id); ?>"><?php echo $category->name; ?></a>
+      </li>
+    <?php endforeach; ?>
   </ul>
   <h4><i class="bi bi-tags me-2"></i>タグ</h4>
-  <ul class="list-unstyled">
+  <ul class="d-flex flex-wrap list-unstyled">
     <?php wp_tag_cloud(); ?>
   </ul>
   <h4><i class="bi bi-person me-2"></i>プロフィール</h4>
   <p>
-    <img src="https://old.hyrorre.com/wp-content/uploads/2018/01/profile-300x300.jpg" alt="Profile Image"
-      style="width: 150px" class="m-2" />
+    <img src="https://old.hyrorre.com/wp-content/uploads/2018/01/profile-300x300.jpg" alt="Profile Image" style="width: 150px" class="m-2" />
   </p>
   <p>hyrorre - すたろあ</p>
   <p>音ゲーマー 兼 Webエンジニア</p>
