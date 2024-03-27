cdaf33e9398f4d95676966e70d5d6c727787e304
hyrorre <hiromu.yamashita@hotmail.com>
2024-03-27 18:01:36 +0900

    Disable: google-recapcha in non-contact pages

--- functions.php
+++ functions.php
@@ -18,6 +18,8 @@ add_action('wp_enqueue_scripts', function () {
   if (!is_page('contact')) {
     wp_dequeue_style('contact-form-7');
     wp_dequeue_script('contact-form-7');
+    wp_deregister_script('google-recaptcha');
+    wp_deregister_script('google-invisible-recaptcha');
   }
 });
 
